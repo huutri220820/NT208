b40f66a8f8739466c94593d3e806140f1ddb4799
huutri220820 <huutri220820@gmail.com>
2020-10-12 01:44:05 +0700

    .

--- WebApplication/Startup.cs
+++ WebApplication/Startup.cs
@@ -31,7 +31,6 @@ namespace WebApplication
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             //sqlserver
             services.AddDbContext<EShopDbContext>(option =>
                 option.UseLoggerFactory(MyLoggerFactory)
